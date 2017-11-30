8cc70b61e9f120da464a677e04b1552945badb22
Yusuf Hendricks <yhendricks1@gmail.com>
2017-11-30 22:16:16 +0200

    cleaning up code

--- humitemp.py
+++ humitemp.py
@@ -4,17 +4,21 @@
 # description     :This program controls and logs the temperature and humidity
 # author          :
 # date            :
-# version         :0.01
+# version         :0.02
 # usage           :python humitemp.py
 # notes           :
 # python_version  :2.7.6
 # =======================================================================
 
 # Import the modules needed to run the script.
-import sys, os, threading, logging
+import logging
+import os
+import sys
+import threading
 from datetime import datetime, timedelta
-from time import sleep
 from logging.handlers import RotatingFileHandler
+from time import sleep
+
 try:
     from configparser import ConfigParser
 except ImportError:
@@ -34,26 +38,14 @@ HUMIDITY_CONTROL = 2                    # GPIO_2
 TEMPERATURE_PORT = '5'                  # A5
 HUMIDITY_PORT = '3'                     # A3
 
-# Main definition - constants
-# mainmenu_actions = {}
-# configmenu_action = {}
-
 # Global Variables
 cycle_started = False
 preheat_started = False
-# cycle_1_enabled = True
-# cycle_2_enabled = True
-# max_temperature_1 = 50
-# max_temperature_2 = 50
-# max_humidity_1 = 95
-# max_humidity_2 = 70
 current_cycle_duration_in_minutes = 1440  # minutes
-# cycle_2_duration_in_minutes = 1440  # minutes
 current_cycle_start_time = datetime.now()  # tbd
 current_cycle_index = 0
-# cycle_2_start_time = datetime.now()  # tbd
 last_temperature = (1, 1, 1)
-last_humidity = (1,1,1)
+last_humidity = (1, 1, 1)
 temperature_element_on = False
 humidity_element_on = False
 control_thread = threading.Thread()         # target=measure_and_control
@@ -66,15 +58,19 @@ HUMIDITY_HYSTERESIS = 1
 
 
 class CycleConfig:
-    enabled = True
-    max_temperature = 50
-    max_humidity = 95
-    cycle_duration_in_minutes = 1440
+    def __init__(self, enabled, max_temperature, max_humidity, cycle_duration_in_minutes):
+        self.enabled = enabled
+        self.max_temperature = max_temperature
+        self.max_humidity = max_humidity
+        self.cycle_duration_in_minutes = cycle_duration_in_minutes
+        pass
+
     cycle_start_time = datetime.now()
 
 
 def measure_and_control():
-    global last_temperature, last_humidity, cycle_started, current_cycle_duration_in_minutes, current_cycle_start_time, current_cycle_index
+    global last_temperature, last_humidity, cycle_started, current_cycle_duration_in_minutes
+    global current_cycle_index, current_cycle_start_time
 
     next_check = datetime.now()
     next_log = datetime.now() + timedelta(seconds=10)
@@ -84,15 +80,17 @@ def measure_and_control():
             current_cycle_start_time = datetime.now()
             cycle.cycle_start_time = current_cycle_start_time
             app_log.info('cycle {} started on {} with duration of {}'
-                         .format(index, current_cycle_start_time.now().strftime("%Y-%m-%d %H:%M:%S"), cycle.cycle_duration_in_minutes ))
+                         .format(index, current_cycle_start_time.now()
+                                 .strftime("%Y-%m-%d %H:%M:%S"), cycle.cycle_duration_in_minutes))
 
         while cycle_started or preheat_started:
             current_cycle_duration_in_minutes = cycle.cycle_duration_in_minutes
-            if cycle_started and datetime.now() > cycle.cycle_start_time + timedelta(minutes=cycle.cycle_duration_in_minutes):
-                app_log.info('cycle {} completed on {}'.format(index, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-                if index >= len(cycle_config):
-                    cycle_started = False
-                break
+            if cycle_started:
+                if datetime.now() > cycle.cycle_start_time + timedelta(minutes=cycle.cycle_duration_in_minutes):
+                    app_log.info('cycle {} completed on {}'.format(index, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+                    if index >= len(cycle_config):
+                        cycle_started = False
+                    break
 
             last_temperature = measure_temperature()
             last_humidity = measure_humidity()
@@ -124,6 +122,8 @@ def measure_and_control():
 # =======================
 #     MENUS FUNCTIONS
 # =======================
+
+
 def main_menu_options():
     clear_screen()
     display_state()
@@ -196,7 +196,7 @@ def clear_screen():
 def display_state():
     global current_cycle_start_time, current_cycle_index
     print '======================='
-    print 'humitemp.py v0.01'
+    print 'humitemp.py v0.02'
     print 'Configuration Settings:'
     print '======================='
     for index, cycle in enumerate(cycle_config):
@@ -213,8 +213,9 @@ def display_state():
             print '============'
             print 'Cycle State:'
             print '============'
-            print '     Cycle {} started on {}'.format(current_cycle_index, current_cycle_start_time.strftime("%Y-%m-%d %H:%M:%S"))
-            print '     Cycle running for {:.0f} minutes with {:.2f}% completed '.format(minutes, perc)
+            print '     Cycle {} started on {}'\
+                .format(current_cycle_index, current_cycle_start_time.strftime("%Y-%m-%d %H:%M:%S"))
+            print '     Cycle {} running for {:.0f} minutes with {:.2f}% completed '.format(current_cycle_index, minutes, perc)
         else:
             print '==================='
             print 'Preheating Chamber:'
@@ -231,20 +232,20 @@ def display_state():
 def log_config():
     for index, cycle in enumerate(cycle_config):
         app_log.info('[configuration{}] - max_temperature = {}, max_humidity = {}, cycle_duration_in_minutes = {}'
-                 .format(index, cycle.max_temperature, cycle.max_humidity, cycle.cycle_duration_in_minutes))
+                     .format(index, cycle.max_temperature, cycle.max_humidity, cycle.cycle_duration_in_minutes))
 
 
 # Configuration Menu
 def config_menu_options():
-    if not cycle_started:
-        print "Configuration Menu:\n"
-        print '1. Set Max Temperature in deg Celcius({})'.format(max_temperature_1)
-        print '2. Set Max Humidity({})'.format(max_humidity_2)
-        print '3. Set Cycle Duration in minutes({})'.format(cycle_1_duration_in_minutes)
-        print "4. Save the configuration to file"
-        print "9. Back"
-        choice = raw_input(" >>  ")
-        config_menu(choice)
+    # if not cycle_started:
+    #     print "Configuration Menu:\n"
+    #     print '1. Set Max Temperature in deg Celcius({})'.format(max_temperature_1)
+    #     print '2. Set Max Humidity({})'.format(max_humidity_2)
+    #     print '3. Set Cycle Duration in minutes({})'.format(cycle_1_duration_in_minutes)
+    #     print "4. Save the configuration to file"
+    #     print "9. Back"
+    #     choice = raw_input(" >>  ")
+    #     config_menu(choice)
     return
 
 
@@ -291,66 +292,69 @@ def exit_program():
 
 
 def set_temp():
-    global max_temperature_1
-    temp = -1
-    while temp < 0 or temp > 80:
-        print "Enter the maximum value for the temperature (0-100 deg): "
-        try:
-            temp = int(raw_input(" >>  "))
-        except:
-            temp = -1
-
-    max_temperature_1 = temp
-    clear_screen()
-    display_state()
+    # global max_temperature_1
+    # temp = -1
+    # while temp < 0 or temp > 80:
+    #     print "Enter the maximum value for the temperature (0-100 deg): "
+    #     try:
+    #         temp = int(raw_input(" >>  "))
+    #     except:
+    #         temp = -1
+    #
+    # max_temperature_1 = temp
+    # clear_screen()
+    # display_state()
     configmenu_actions['config_menu']()
 
 
 def set_hum():
-    global max_humidity_2
-    hum = -1
-    while hum < 0 or hum > 95:
-        print "Enter the maximum percentage value for the humidity (0-100): "
-        try:
-            hum = int(raw_input(" >>  "))
-        except:
-            hum = -1
-
-    max_humidity_2 = hum
-    clear_screen()
-    display_state()
-    configmenu_actions['config_menu']()
+    # global max_humidity_2
+    # hum = -1
+    # while hum < 0 or hum > 95:
+    #     print "Enter the maximum percentage value for the humidity (0-100): "
+    #     try:
+    #         hum = int(raw_input(" >>  "))
+    #     except:
+    #         hum = -1
+    #
+    # max_humidity_2 = hum
+    # clear_screen()
+    # display_state()
+    # configmenu_actions['config_menu']()
+    pass
 
 
 def set_duration():
-    global cycle_1_duration_in_minutes
-    duration = -1
-    while duration < 0 or duration > 1440:
-        print "Enter the cycle duration (0-1440 minutes): "
-        try:
-            duration = int(raw_input(" >>  "))
-        except:
-            duration = -1
-
-    cycle_1_duration_in_minutes = duration
-    clear_screen()
-    display_state()
-    configmenu_actions['config_menu']()
+    # global cycle_1_duration_in_minutes
+    # duration = -1
+    # while duration < 0 or duration > 1440:
+    #     print "Enter the cycle duration (0-1440 minutes): "
+    #     try:
+    #         duration = int(raw_input(" >>  "))
+    #     except:
+    #         duration = -1
+    #
+    # cycle_1_duration_in_minutes = duration
+    # clear_screen()
+    # display_state()
+    # configmenu_actions['config_menu']()
+    pass
 
 
 def save_config():
-    config = ConfigParser()  # instantiate
-    config.read('config.ini')
-    # update existing values
-    config.set('configuration', 'max_temperature', max_temperature_1)
-    config.set('configuration', 'max_humidity', max_humidity_2)
-    config.set('configuration', 'cycle_duration_in_minutes', cycle_1_duration_in_minutes)
-    # save to  file
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
-    log_config()
-    print "Configuration saved to config.ini\n"
-    configmenu_actions['config_menu']()
+    # config = ConfigParser()  # instantiate
+    # config.read('config.ini')
+    # # update existing values
+    # config.set('configuration', 'max_temperature', max_temperature_1)
+    # config.set('configuration', 'max_humidity', max_humidity_2)
+    # config.set('configuration', 'cycle_duration_in_minutes', cycle_1_duration_in_minutes)
+    # # save to  file
+    # with open('config.ini', 'w') as configfile:
+    #     config.write(configfile)
+    # log_config()
+    # print "Configuration saved to config.ini\n"
+    # configmenu_actions['config_menu']()
+    pass
 
 
 def read_port(channel):
@@ -364,7 +368,7 @@ def read_port(channel):
 def read_temp_and_hum_pins():
     # fudge factor at approx 55deg is to minus 6.75% from value read
     while 1:
-        temperature, avg_val, fudge_avg_val  = measure_temperature()
+        temperature, avg_val, fudge_avg_val = measure_temperature()
         print "ADC Channel: {}, Raw Result: {}, Fudge Result: {}, Fudge Temp: {:.1f} deg".\
             format(TEMPERATURE_PORT, avg_val, fudge_avg_val, temperature)
         humidity, avg_val, fudge_avg_val = measure_humidity()
@@ -376,7 +380,7 @@ def read_temp_and_hum_pins():
 def set_gpio_pin():
     try:
         pin = int(raw_input("Enter pin number >>  "))
-        if pin >= 0 and pin <= 17:  # up to 17 gpio pins on pcduino3
+        if 0 <= pin <= 17:  # up to 17 gpio pins on pcduino3
             choice = int(raw_input("1 for HIGH or 2 for LOW >>  "))
             if choice == 1:
                 set_pin(pin, HIGH)
@@ -398,7 +402,7 @@ mainmenu_actions = {
     '3': preheat_toggle,
     '4': read_temp_and_hum_pins,
     '5': set_gpio_pin,
-    #'8': menu2,
+    # '8': menu2,
     '0': exit_program,
 }
 
@@ -416,26 +420,27 @@ configmenu_actions = {
 def measure_temperature():
     # assuming a range of -30 to 70, then a factor of 40.95 units represents 1 deg, with an offset of -30
     # fudge factor at approx 55deg is to minus 6.75% from value read
-    NUM_AVG_VALUES = 10
+    num_avg_values = 10
     values = []
-    for x in range(0, NUM_AVG_VALUES):
+    for x in range(0, num_avg_values):
         values.append(read_port(TEMPERATURE_PORT))
     avg_val = sum(values)/len(values)
     fudge_avg_val = avg_val - int(avg_val * TEMPERATURE_FUDGE_FACTOR)
     # print 'Temperature = {}'.format((fudge_avg_val / STEPS_PER_DEGREE) - TEMPERATURE_OFFSET)
-    return (fudge_avg_val / STEPS_PER_DEGREE) - TEMPERATURE_OFFSET, avg_val, fudge_avg_val    # returns 0 - 4095  for a range of 0V to 3.3
+    # ADC returns 0 - 4095  for a range of 0V to 3.3
+    return (fudge_avg_val / STEPS_PER_DEGREE) - TEMPERATURE_OFFSET, avg_val, fudge_avg_val
 
 
 def measure_humidity():
     # assuming a range of 0 - 100% RH, then a factor of 40.95 units represents 1 percentage of humidity
-    NUM_AVG_VALUES = 10
+    num_avg_values = 10
     values = []
-    for x in range(0, NUM_AVG_VALUES):
+    for x in range(0, num_avg_values):
         values.append(read_port(HUMIDITY_PORT))
     avg_val = sum(values)/len(values)
     fudge_avg_val = avg_val - int(avg_val * HUMIDITY_FUDGE_FACTOR)
-
-    return (fudge_avg_val / STEPS_PER_DEGREE), avg_val, fudge_avg_val,     # returns 0 - 4095  for a range of 0V to 3.3
+    # ADC returns 0 - 4095  for a range of 0V to 3.3
+    return (fudge_avg_val / STEPS_PER_DEGREE), avg_val, fudge_avg_val,
 
 
 def set_temperature_element(state):
@@ -479,10 +484,9 @@ def configure_pins(pin_list, pin_type):
 #      MAIN PROGRAM
 # =======================
 if __name__ == "__main__":
-    #init()          # this is required for colorama on windows
     # setup logging
     # log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
-    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    log_formatter = logging.Formatter('%(asctime)s, %(levelname)s, %(message)s')
     logFile = 'temperature_and_humidity_log.txt'
     my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=10 * 1024 * 1024,
                                      backupCount=10, encoding=None, delay=0)
@@ -504,22 +508,22 @@ if __name__ == "__main__":
     # read values from the "configuration" section
     cycle_config = []
     try:
-        cycle1 = CycleConfig()
-        cycle1.max_temperature = config.getint('configuration0', 'max_temperature')
-        cycle1.max_humidity = config.getint('configuration0', 'max_humidity')
-        cycle1.cycle_duration_in_minutes = config.getint('configuration0', 'cycle_duration_in_minutes')
-        cycle1.cycle_enabled = True
-        cycle_config.append(cycle1)
+        cycle_config.append(CycleConfig(
+            True,
+            config.getint('configuration0', 'max_temperature'),
+            config.getint('configuration0', 'max_humidity'),
+            config.getint('configuration0', 'cycle_duration_in_minutes'),
+        ))
     except:
         app_log.info("Cycle 0 configuration error")
 
     try:
-        cycle2 = CycleConfig()
-        cycle2.max_temperature = config.getint('configuration1', 'max_temperature')
-        cycle2.max_humidity = config.getint('configuration1', 'max_humidity')
-        cycle2.cycle_duration_in_minutes = config.getint('configuration1', 'cycle_duration_in_minutes')
-        cycle2.enabled = True
-        cycle_config.append(cycle2)
+        cycle_config.append(CycleConfig(
+            True,
+            config.getint('configuration1', 'max_temperature'),
+            config.getint('configuration1', 'max_humidity'),
+            config.getint('configuration1', 'cycle_duration_in_minutes')
+        ))
     except:
         app_log.info("Cycle 1 configuration error")
 
@@ -542,7 +546,8 @@ if __name__ == "__main__":
 # req=[[6,0,0], [6,64,0],[6,128,0],[6,192,0],[7,0,0], [7,64,0],[7,128,0],[7,192,0]]
 #
 # # list is based on 'r = spi.xfer2([6|(adcnum>>2),(adcnum&3)<<6,0])' which in turn was based on readadc12 from
-# # http://git.agocontrol.com/apagg/agocontrol-work/blob/93f7d9051aebf79c794e3d5fa4feea92c9221831/raspiMCP3xxxGPIO/MCP3208.py
+# # http://git.agocontrol.com/apagg/agocontrol-work/blob
+#           /93f7d9051aebf79c794e3d5fa4feea92c9221831/raspiMCP3xxxGPIO/MCP3208.py
 #
 # # choose the current SPI device (one of eight)
 # def spi_device(devno):
@@ -597,5 +602,3 @@ if __name__ == "__main__":
 
 
 # ------------------------------------------------------------------------------------------------------
-
-
